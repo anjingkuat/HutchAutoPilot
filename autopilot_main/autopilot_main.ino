b8b766e475e962a376dbefe896be2e1e005f7712
anjingkuat <jonathan.mathews@mines.sdsmt.edu>
2015-11-23 21:45:06 -0600

    Flowchart, Pseudocode, and Project Summary File
    The first three files of this repository, so that people can look at
    them and contribute if they want to.

---
+++ autopilot_main/autopilot_main.ino
@@ -0,0 +1,38 @@
+//include the necessary libraries
+//initialize any global variables
+
+void setup() {
+  //begin communicating over serial and I2C
+  //set up the sensors
+    //set up the barometer, bmp180
+    //set up the IMU, mpu6050
+    //set up the magnetometer, hmc5883l
+    //set up the gps
+    //set up the airspeed sensor
+  //ensure the aircraft is not moving and zero all the sensors
+  //determine initial location and orientation
+  //output initialization status (over serial or using leds)
+}
+
+void loop() {
+  //The following items must be performed, but not at the same frequency
+    //interpret external commands
+      //determine flight mode (manual, assisted manual, gps waypoint, dead reckoning, 
+      //  path following, object following, return to home, etc)
+      //gather necessary inputs for current mode
+    //update desired location and orientation
+      //pass the inputs for the current mode to the function for that mode
+      //the function will return a desired location and orientation
+    //gather sensor data
+      //get baro
+      //get IMU
+      //get mag
+      //get gps
+      //get airspeed
+    //update location and orientation estimate
+      //run sensor fusion for orientation
+      //run kalman for location
+    //update desired location and orientation
+    //determine necessary control actuations
+    //actuate controls
+}
